b54444885904441992fcb8107f42f1f5f0bb0b65
Aurus Soares <neiven131@gmail.com>
2024-11-29 23:20:00 -0300

    feat: Add the initial project files

---
+++ android/app/src/main/kotlin/br/com/granago/granago_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package br.com.granago.granago_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/app_widget.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:granago_app/src/pages/home_page.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'GranaGo',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
