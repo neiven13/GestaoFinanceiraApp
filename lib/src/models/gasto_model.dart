561c5c3798f849bed5075226828e9d6ce7984900
Aurus Soares <neiven131@gmail.com>
2024-12-09 21:39:55 -0300

    refactor: make GastoModel attributes private and adjust access

--- lib/src/models/gasto_model.dart
+++ lib/src/models/gasto_model.dart
@@ -1,9 +1,40 @@
 class GastoModel {
-  late int id;
-  late String descricao;
-  late DateTime dataGasto;
-  late double valorGasto;
+  late int _id;
+  late String _descricao;
+  late DateTime _dataGasto;
+  late double _valorGasto;
 
-  GastoModel(this.id, this.descricao, this.dataGasto, this.valorGasto);
+  GastoModel({
+    required int id,
+    required String descricao,
+    required DateTime dataGasto,
+    required double valorGasto,
+  }) {
+    _id = id;
+    _descricao = descricao;
+    _dataGasto = dataGasto;
+    _valorGasto = valorGasto;
+  }
+
+  int get id => _id;
+  String get descricao => _descricao;
+  DateTime get dataGasto => _dataGasto;
+  double get valorGasto => _valorGasto;
+
+  set id(int value) {
+    _id = value;
+  }
+
+  set descricao(String value) {
+    _descricao = value;
+  }
+
+  set dataGasto(DateTime value) {
+    _dataGasto = value;
+  }
+
+  set valorGasto(double value) {
+    _valorGasto = value;
+  }
 }
 
