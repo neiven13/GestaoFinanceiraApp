b441e593e84fe8d8a9f19a64db851fc2842cbddb
Aurus Soares <neiven131@gmail.com>
2024-12-11 22:23:49 -0300

    refactor: add toMap method and change attribute encapsulation

--- lib/src/models/gasto_model.dart
+++ lib/src/models/gasto_model.dart
@@ -1,40 +1,26 @@
 class GastoModel {
-  late int _id;
-  late String _descricao;
-  late DateTime _dataGasto;
-  late double _valorGasto;
+  int? id;
+  String descricao;
+  DateTime dataGasto;
+  double valorGasto;
 
   GastoModel({
-    required int id,
-    required String descricao,
-    required DateTime dataGasto,
-    required double valorGasto,
-  }) {
-    _id = id;
-    _descricao = descricao;
-    _dataGasto = dataGasto;
-    _valorGasto = valorGasto;
-  }
+    this.id,
+    required this.descricao,
+    required this.dataGasto,
+    required this.valorGasto,
+  });
 
-  int get id => _id;
-  String get descricao => _descricao;
-  DateTime get dataGasto => _dataGasto;
-  double get valorGasto => _valorGasto;
+  Map<String, dynamic> toMap() {
+    final map = <String, dynamic>{
+      'descricao': descricao,
+      'dataGasto': dataGasto.toIso8601String(),
+      'valorGasto': valorGasto,
+    };
+    if (id != null) {
+      map['id'] = id;
+    }
 
-  set id(int value) {
-    _id = value;
-  }
-
-  set descricao(String value) {
-    _descricao = value;
-  }
-
-  set dataGasto(DateTime value) {
-    _dataGasto = value;
-  }
-
-  set valorGasto(double value) {
-    _valorGasto = value;
+    return map;
   }
 }
-
